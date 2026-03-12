b526dc0fa5870d6de49d8963df0c36459856aec2
Normchell <vgavrilov@fmschool72.ru>
2026-03-12 22:32:08 +0300

    I am lost how to make Move class make sense ); HELP!!!

--- src/flip7_logic/Move.py
+++ src/flip7_logic/Move.py
@@ -1,9 +1,14 @@
+from typing import List
+
 from .Card import Card
 from .Player import Player
 
 class Move:
-    def __init__(self, target_player: Player, card: Card, current_turn: int, next_turn: int):
-        self.target_plater = target_player
-        self.card = card
-        self.current_turn = current_turn
-        self.next_turn = next_turn
+    def __init__(
+        self,
+        is_hit: bool,
+        current_turn: int,
+        next_turn: int,
+        target_player: Player = None, # This might be setup after the move was generated
+    ):
+        pass
--- src/flip7_logic/State.py
+++ src/flip7_logic/State.py
@@ -3,6 +3,7 @@ from typing import List
 from .Deck import Deck
 from .Deck import Deck as Discard # TODO: Make a custom class for this
 from .Player import Player
+from .Move import Move
 
 class State:
     def __init__(
@@ -17,3 +18,18 @@ class State:
         self.players = players
         self.discard = discard
         self.turn = turn
+
+
+    def generate_legal_moves(self) -> List[Move]:
+        next_active_player = self.turn + 1
+
+        while next_active_player != self.turn:
+            if self.players[next_active_player].is_active:
+                break
+
+            next_active_player += 1
+            next_active_player %= self.players.__len__()
+
+        if not self.players[next_active_player].is_active:
+            return []
+
